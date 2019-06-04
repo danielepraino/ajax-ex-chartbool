461013fcb74f2971457ad232c75b032a6c107921
Daniele Praino <daniele.praino@gmail.com>
2019-06-04 20:40:00 +0200

    chartbool_v1 milestone 1 work in progress

--- chartbool_v1/script.js
+++ chartbool_v1/script.js
@@ -2,16 +2,11 @@ moment.locale("it");
 
 var apiURL = "http://157.230.17.132:4014/sales/";
 var totalSales = {};
-var data = [];
-var labels = [];
-
-var ctx = document.getElementById('myLineChart').getContext('2d');
-
-var myLineChart = new Chart(ctx, {
-    type: 'line',
-    data: data,
-    options: ""
-});
+var singleSales = {};
+var dataTotalSales = [];
+var dataSingleSales = [];
+var labelSingleSales = [];
+var allSales = 0;
 
 function call_API(){
   $.ajax({
@@ -23,20 +18,30 @@ function call_API(){
       for (var i = 0; i < obj.length; i++) {
         var formattedData = moment(obj[i].date, "DD/MM/YYYY").format("YYYY/MM/DD");
         var currentMonth = moment(formattedData).month();
+        var employee = obj[i].salesman;
         var employeeSale = obj[i].amount;
         var months = Object.keys(totalSales);
-        console.log(months);
-        console.log(currentMonth);
+        var men = Object.keys(singleSales);
         if (!months.includes(currentMonth)) {
-          totalSales[currentMonth] = 0;
+          totalSales[currentMonth] = employeeSale;
         } else {
           totalSales[currentMonth] += employeeSale;
         }
+        if (!men.includes(employee)) {
+          singleSales[employee] = employeeSale;
+        } else {
+          singleSales[employee] += employeeSale;
+        }
+        allSales += employeeSale;
       }
-      var data = Object.values(totalSales);
-      console.log(totalSales);
+      labelSingleSales = Object.keys(singleSales);
+      dataTotalSales = Object.values(totalSales);
+      for (var j = 0; j < 4; j++) {
+        dataSingleSales.push(Math.ceil((Object.values(singleSales)[j]) / allSales * 100));
+      }
+      console.log(dataSingleSales);
+      console.log(labelSingleSales);
       console.log(obj);
-      console.log("data: " + data);
     },
     error: function() {
       alert("errore");
@@ -45,3 +50,28 @@ function call_API(){
 }
 
 call_API();
+
+//var ctx = document.getElementById("lineChart").getContext("2d");
+var ctx2 = document.getElementById("doughnutChart").getContext("2d");
+
+// var lineChart = new Chart(ctx, {
+//     type: 'line',
+//     data: {
+//         labels: [labelTotalSales],
+//         datasets: [{
+//             data: [10, 20],
+//         }]
+//     },
+//     options: ""
+// });
+
+var doughnutChart = new Chart(ctx2, {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: dataSingleSales,
+        }],
+        labels: labelSingleSales,
+    },
+    options: ""
+});
