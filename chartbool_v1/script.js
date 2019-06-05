898ed39bfc478df6cbd13c19e4955202e3481d85
Daniele Praino <daniele.praino@gmail.com>
2019-06-05 16:26:57 +0200

    chartbool_v2 milestone 2 work in progress

--- chartbool_v1/script.js
+++ chartbool_v1/script.js
@@ -2,11 +2,16 @@ moment.locale("it");
 
 var apiURL = "http://157.230.17.132:4014/sales/";
 var singleSales = {};
+var workForce = {};
 var dataTotalSales = [];
 var dataSingleSales = [];
 var labelTotalSales = [];
 var labelSingleSales = [];
 var allSales = 0;
+
+//setto le variabili per handlebarsjs
+var optionTemplate = $("#option-template").html();
+var selectTemplate = Handlebars.compile(optionTemplate);
 
 var months = {
   gennaio: 0,
@@ -23,6 +28,40 @@ var months = {
   dicembre: 0
 };
 
+function dataSalesPerMonth(obj) {
+  for (var i = 0; i < obj.length; i++) {
+    var formattedData = moment(obj[i].date, "DD/MM/YYYY");
+    var currentMonth = formattedData.format("MMMM");
+    var employee = obj[i].salesman;
+    var employeeSale = obj[i].amount;
+    months[currentMonth] += employeeSale;
+  }
+  labelTotalSales = Object.keys(months);
+  dataTotalSales = Object.values(months);
+}
+
+function dataSalesPerEmployee(obj) {
+  for (var i = 0; i < obj.length; i++) {
+    var employee = obj[i].salesman;
+    var employeeSale = obj[i].amount;
+    workForce = Object.keys(singleSales);
+    if (!workForce.includes(employee)) {
+      singleSales[employee] = employeeSale;
+    } else {
+      singleSales[employee] += employeeSale;
+    }
+    allSales += employeeSale;
+  }
+  labelSingleSales = Object.keys(singleSales);
+  if (allSales > 0) {
+    for (var employee in singleSales) {
+      dataSingleSales.push(((singleSales[employee] / allSales) * 100).toFixed(1));
+      $(".employees").append(selectTemplate(workForce));
+      console.log(workForce);
+    }
+  }
+}
+
 function call_API(){
   $.ajax({
     url: apiURL,
@@ -30,32 +69,8 @@ function call_API(){
     data: {
     },
     success: function(obj){
-      for (var i = 0; i < obj.length; i++) {
-        var formattedData = moment(obj[i].date, "DD/MM/YYYY");
-        var currentMonth = formattedData.format("MMMM");
-        console.log(currentMonth);
-        months[currentMonth] += employeeSale;
-        console.log(months);
-        var employee = obj[i].salesman;
-        var employeeSale = obj[i].amount;
-
-        //grafico vendite venditori
-        var men = Object.keys(singleSales);
-        if (!men.includes(employee)) {
-          singleSales[employee] = employeeSale;
-        } else {
-          singleSales[employee] += employeeSale;
-        }
-        allSales += employeeSale;
-      }
-      labelSingleSales = Object.keys(singleSales);
-      labelTotalSales = Object.keys(months);
-      dataTotalSales = Object.values(months);
-      if (allSales > 0) {
-        for (var employee in singleSales) {
-          dataSingleSales.push(((singleSales[employee] / allSales) * 100).toFixed(1));
-        }
-      }
+      dataSalesPerMonth(obj);
+      dataSalesPerEmployee(obj);
       console.log(obj);
       drawLineChart(labelTotalSales, dataTotalSales);
       drawDoughnutChart(labelSingleSales, dataSingleSales);
@@ -73,8 +88,23 @@ function drawLineChart(label, data) {
     data: {
       labels: labelTotalSales,
       datasets: [{
-          data: dataTotalSales,
+        label: "Vendite",
+        data: dataTotalSales,
+        backgroundColor: ["#e67e22"],
+        borderColor: ["#d35400"],
+        borderWidth: 5
       }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Vendite totali nel 2017"
+      },
+      elements: {
+          line: {
+              tension: 0
+          }
+      }
     }
   });
 }
